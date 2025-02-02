2c8d103f7736404c318f51487812b491dde85949
Raymart Magallanes <magallanesraymart@gmail.com>
2025-02-02 15:10:47 +0800

    added tailwind design

--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -13,24 +13,24 @@ defineProps({
 <template>
 
     <div class="min-h-screen w-full flex flex-col items-center gap-3 py-5">
-        <h1 class="text-4xl text-center font-semibold ">Categories</h1>
+        <h1 class="text-4xl text-center font-semibold">Categories</h1>
         <table class="table-fixed border-collapse border border-slate-400 p-2">
-            <thead class="bg-green-500 text-white/80">
+            <thead class="bg-blue-400 text-white/80">
                 <tr>
-                    <th>Name</th>
-                    <th>Description</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Name</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Description</th>
                 </tr>
             </thead>
 
             <tbody>
                 <tr v-for="category in categories" :key="category.id">
-                    <td>{{ category.name }}</td>
-                    <td>{{ category.description }}</td>
+                    <td class="px-6 py-4 border-b border-gray-500 text-sm">{{ category.name }}</td>
+                    <td class="px-6 py-4 border-b border-gray-500 text-sm">{{ category.description }}</td>
                 </tr>
             </tbody>
         </table>
 
-        <Link href="/products" class="hover:text-sky-800 hover:underline duration-100">Go to Products</Link>
+        <Link href="/products" class="hover:text-sky-800 hover:underline duration-100 text-sm">Go to Products</Link>
     </div>
 
 
--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -21,13 +21,13 @@ defineProps( {
 
 <template>
     <div class="flex">
-        <nav class="min-h-screen w-[20%] shadow-lg">
+        <nav class="min-h-screen w-[20%] shadow-lg border-r border-gray-200">
             <h2 class="text-center text-2xl font-semibold my-3">
-                <Link href="/products" prefetch>Categories</Link>
+                <Link href="/products" prefetch class="hover:underline">Categories</Link>
             </h2>
             <ul>
-                <li v-for="category in categories" :key="category.id" class="p-2 cursor-pointer hover:bg-slate-600 hover:text-slate-300 duration-50">
-                    <Link :href='"/products/"+ category.id' class="w-full" :preserve-state="true" prefetch>
+                <li v-for="category in categories" :key="category.id" class="px-6 py-4 cursor-pointer hover:bg-gray-500 hover:text-slate-300 duration-50 text-sm">
+                    <Link :href='"/products/"+ category.id' class="block w-full" :preserve-state="true" prefetch>
                         <div class="w-full">
                             {{ category.name }}
                         </div>
@@ -37,12 +37,11 @@ defineProps( {
         </nav>
 
 
-
         <div class="w-3/4 px-3">
             <h1  class="text-center text-2xl font-semibold my-3">{{ selectedCategory? selectedCategory.name: "All Products" }}</h1>
             <div class="grid grid-cols-3 gap-3">
                 <div v-for="product in products" :key="product.id">
-                   <div class="shadow-lg p-2 rounded-lg animation-bounce">
+                   <div class="shadow-lg p-2 rounded-lg animation-bounce text-sm hover:bg-gray-400 duration-500 cursor-pointer">
                     <h5 class="text-lg font-semibold"> {{ product.name }}</h5>
                     <p>{{ product.description  }}</p>
                     <div class="flex justify-between">
